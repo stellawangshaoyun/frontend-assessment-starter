1a8dd6f1b4d42ade698c7b45638b5c1070c89cb1
Stella_Wang <62366397+stellawangshaoyun@users.noreply.github.com>
2025-09-25 14:01:09 +0100

    setting for radix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,35 @@
+const { violet, blackA, mauve, green, gray } = require("@radix-ui/colors");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+				...mauve,
+				...violet,
+				...green,
+				...blackA,
+				...gray,
+			},
       animation: {
         "fade-in": "fadeIn 0.3s ease-in-out",
         "slide-up": "slideUp 0.3s ease-out",
         "bounce-subtle": "bounceSubtle 0.4s ease-out",
+        overlayShow: "overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+				contentShow: "contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
       },
       keyframes: {
+        overlayShow: {
+					from: { opacity: "0" },
+					to: { opacity: "1" },
+				},
+				contentShow: {
+					from: {
+						opacity: "0",
+						transform: "translate(-50%, -48%) scale(0.96)",
+					},
+					to: { opacity: "1", transform: "translate(-50%, -50%) scale(1)" },
+				},
         fadeIn: {
           "0%": { opacity: "0" },
           "100%": { opacity: "1" },
